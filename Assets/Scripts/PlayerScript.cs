48e1ae259e24d110eeabe5134c1d934f48d3cf52
abhishekjoshigamerz <69464702+abhishekjoshigamerz@users.noreply.github.com>
2020-09-25 12:55:28 +0530

    2d Platformer game - assignment

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -4,23 +4,27 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
-    
+      public float sp=5f;
     public Animator Animator;
     public BoxCollider2D boxCollider;
 
-    private float OriginalSizeCollider;
-    private float Valuex;
-    public float crouchHeight;
+    private Vector2 OriginalSizeCollider,OriginalColliderOffset;
+    private Rigidbody2D Playerbody;
     private float resize;
     private bool IsCrouching=false;
+    private bool IsGrounded=true;
+    private bool jumped;
+    public Transform groundCheckPosition;
+    public LayerMask groundLayer;
     // Start is called before the first frame update
     void Start()
     {
        boxCollider = GetComponent<BoxCollider2D>(); 
-
-       OriginalSizeCollider = boxCollider.size.y;
-       Valuex = boxCollider.size.x;
-       Debug.Log(OriginalSizeCollider);
+       Playerbody = GetComponent<Rigidbody2D>();
+       OriginalSizeCollider = boxCollider.size;
+       OriginalColliderOffset = boxCollider.offset;
+        
+       
     }
 
     // Update is called once per frame
@@ -29,49 +33,66 @@ public class PlayerScript : MonoBehaviour
         float speed = Input.GetAxisRaw("Horizontal");
         Animator.SetFloat("Speed", Mathf.Abs(speed));
         ChangeDirection(speed);
-        // float jumpingspeed = Input.GetAxisRaw("Vertical");
-        //  int isjumping = 1;
-        // if(jumpingspeed>0 ){
-           
-        //      Jump(isjumping);
-        // }
-       
-       //now Code for Crouching our Player
-
-
         Crouch();
-       
+        CheckGrounded();
+        Jump();
     }
 
     //Packed all the code in one place to change the direction of Player
     void ChangeDirection(float speed){
          Vector3 scale = transform.localScale; 
         if(speed < 0){   
+            Playerbody.velocity = new Vector2(-sp,Playerbody.velocity.y);
             scale.x = - 1 * Mathf.Abs(scale.x);
             Debug.Log(speed);
         } else if(speed > 0){
+                Playerbody.velocity = new Vector2(sp,Playerbody.velocity.y);
                scale.x = Mathf.Abs(scale.x); 
                Debug.Log(speed);
+        }else{
+            Playerbody.velocity= new Vector2(0,Playerbody.velocity.y);
         }
         transform.localScale = scale;
     }
 
     void Crouch(){
         if(Input.GetKey(KeyCode.LeftControl)){
-            if(IsCrouching==false){
                 IsCrouching= true;
-                Animator.SetTrigger("IsCrouch");
-             //Animator.SetBool("IsCrouch",true);
-             resize = crouchHeight;
-             boxCollider.size = new Vector2(Valuex,resize);  
+                Animator.SetBool("IsCrouch",true);
+            
+            
+             boxCollider.size = new Vector2(boxCollider.size.x,1.2f);
+             boxCollider.offset = new Vector2(boxCollider.offset.x,.6f);  
             }else{
                  IsCrouching= false;
-                    Animator.ResetTrigger("IsCrouch");
-           //      Animator.SetBool("IsCrouch",false);
-                 boxCollider.size = new Vector2(Valuex,OriginalSizeCollider); 
+                   
+           Animator.SetBool("IsCrouch",false);
+                 boxCollider.size = OriginalSizeCollider; 
+                 boxCollider.offset = OriginalColliderOffset;
             }
         }
-    }
+
+
+        void CheckGrounded(){
+          IsGrounded = Physics2D.Raycast(groundCheckPosition.position,Vector2.down,0.1f,groundLayer);
+
+          if(IsGrounded){
+              if(jumped){
+                  jumped = false;
+                  Animator.SetBool("IsJump",false);
+              }
+          }
+        }
+        void Jump(){
+         if(IsGrounded){
+             if(Input.GetKey(KeyCode.W)){
+                 jumped=true;
+                 Animator.SetBool("IsJump",true);
+             }
+         }
+        }
+         
+    
 
    
 }
